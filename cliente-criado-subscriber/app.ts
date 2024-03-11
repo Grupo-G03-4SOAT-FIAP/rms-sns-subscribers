ca5744052ec57903bd68bbb1d726aa193a122b31
Danilo das Neves Dantas <5115895+dannevesdantas@users.noreply.github.com>
2024-03-11 07:11:39 -0300

    feat: Refatora function cliente-criado-subscriber

--- cliente-criado-subscriber/app.ts
+++ cliente-criado-subscriber/app.ts
@@ -2,16 +2,6 @@ import { SNSEvent, SNSEventRecord } from 'aws-lambda';
 import { AmazonCognitoService } from './cognito.service';
 import 'dotenv/config'
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
-
 export const lambdaHandler = async (event: SNSEvent) => {
     for (const record of event.Records) {
         await processMessageAsync(record);
